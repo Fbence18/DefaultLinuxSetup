c5f877002f9c5ae47c905831c0694d3c679c64ba
FBence <90272544+Fbence18@users.noreply.github.com>
2024-12-01 18:47:18 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+
+sudo apt install mc git apache2 snap snapd -y
+
+
---
+++ update.sh
@@ -0,0 +1,9 @@
+
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt full-upgrade -y
+
+sudo apt autoremove 
+
+sudo apt list --upgradable
